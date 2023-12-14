0567b674cbed875afb9888933ea08a75e61de989
Toni Wahrstätter <51536394+nerolation@users.noreply.github.com>
2023-12-14 21:06:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const endpointHeaders = document.querySelectorAll('#code-sample-header');
+    
+    endpointHeaders.forEach(header => {
+        header.addEventListener('click', function() {
+            // Toggle the next two elements (the <p> and the <pre><code>)
+            //var apiSchema = this.nextElementSibling;
+            //var contentDescription = apiSchema.nextElementSibling;
+            var contentCode = this.nextElementSibling;
+
+            //contentDescription.style.display = (contentDescription.style.display === "block" ? "none" : "block");
+            contentCode.style.display = (contentCode.style.display === "block" ? "none" : "block");
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Initialize Highlight.js
+    document.querySelectorAll('pre code').forEach((el) => {
+        hljs.highlightElement(el);
+    });
+});
